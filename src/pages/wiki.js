270f7d6d94b8ca80d457c5f5df5bc3f5ca817ad1
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-01-22 13:34:04 +0300

    remove embed

--- src/pages/wiki.js
+++ src/pages/wiki.js
@@ -4,7 +4,6 @@ import Stories from 'components/About/Stories';
 import Navbar from 'components/Header/Navbar';
 import DataArchivesHeader from 'components/DataArchives/DataArchivesHeader';
 import DataArchives from 'components/DataArchives/DataArchives';
-import EmbedDocumentation from 'components/EmbedDocumentation';
 import PartnerLogos from 'components/PartnerLogos';
 import Footer from 'components/Footer';
 
@@ -14,7 +13,6 @@ function Data() {
       <Navbar />
       <DataArchivesHeader />
       <DataArchives />
-      <EmbedDocumentation />
       <Stories />
       <PartnerLogos />
       <Footer />
