bac619b67415dffe2217e2e26cebe5d5f7418281
Obed Amoasi <obedamoasi@gmail.com>
2021-01-29 11:49:36 +0000

    remove microseconds

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,9 @@
 export function formatDateTime(timestamp) {
   const date = new Date(timestamp).toDateString().split(' ').slice(1).join(' ');
-  const time = new Date(timestamp).toLocaleTimeString();
+  const time = new Date(timestamp).toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
   return { date, time };
 }
 
